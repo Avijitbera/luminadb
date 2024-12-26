8e0885623c55f5b99496ff16d6a8143424e9469a
Avijit bera <avijitbera775@gmail.com>
2024-12-26 09:49:14 +0530

    created set, get command

--- connection_pool.go
+++ connection_pool.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"net"
+	"strconv"
+	"strings"
+	"time"
 )
 
 type ConnectionPool struct {
@@ -26,5 +30,67 @@ func (p *ConnectionPool) Start(db *Database) {
 }
 
 func (p *ConnectionPool) worker(db *Database) {
+	for conn := range p.jobs {
 
+	}
+}
+
+func handleConnection(conn net.Conn, db *Database) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	conn.Write([]byte("Enter credentials (username password): "))
+	authLine, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	authParts := strings.Split(strings.TrimSpace(authLine), " ")
+	if len(authParts) != 2 || !db.Authenticate(authParts[0], authParts[1]) {
+		conn.Write([]byte("Invalid credentials\n"))
+		return
+	}
+	username := authParts[0]
+	conn.Write([]byte("Authenticated\n"))
+	for {
+
+		conn.Write([]byte("> "))
+		commandLine, err := reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		command := strings.Fields(strings.TrimSpace(commandLine))
+		if len(command) == 0 {
+			conn.Write([]byte("Enter credentials (username password): "))
+			continue
+		}
+		switch strings.ToUpper(command[0]) {
+		case "SET":
+			if len(command) < 3 {
+				conn.Write([]byte("Invalid command\n"))
+
+			} else {
+				key, value := command[1], command[2]
+				ttlSeconds, err := strconv.Atoi(command[3])
+				if err != nil {
+					conn.Write([]byte("Invalid TTL\n"))
+					continue
+				}
+				ttl := time.Duration(ttlSeconds) * time.Second
+				db.Set(username, key, value, ttl)
+				conn.Write([]byte("OK\n"))
+			}
+
+		case "GET":
+			if len(command) != 2 {
+				conn.Write([]byte("Invalid command\n"))
+			} else {
+				key := command[1]
+				value, exists := db.Get(username, key)
+				if exists {
+					conn.Write([]byte(value + "\n"))
+				} else {
+					conn.Write([]byte("Key not found\n"))
+				}
+			}
+		}
+	}
 }
